21852f6ff30d6af115e6b96184d75ce6d9c9e287
刘平峰 <9665730@qq.com>
2020-01-27 21:44:03 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
